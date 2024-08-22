02be730d0624c8e2377cc50740f866a097b29df6
Szxro <szxro@gmail.com>
2024-08-22 18:39:45 -0400

    Add barrel file for the base folder

---
+++ src/base/index.ts
@@ -0,0 +1,15 @@
+import { BaseCommand } from "./base-command";
+import { BaseEvent } from "./base-event";
+import { BaseJob } from "./base-job"
+
+import { QueueNode } from "./queue-node"
+import { Queue } from "./queue"
+
+export {
+    BaseCommand,
+    BaseEvent,
+    BaseJob,
+    QueueNode,
+    Queue
+};
+
