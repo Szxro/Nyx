e10453a857e8f37822b9722b096831e429659a27
Szxro <szxro@gmail.com>
2025-03-10 09:36:04 -0400

    Add logger service implementation

---
+++ src/services/logger-service.ts
@@ -0,0 +1,74 @@
+import { createLogger, format, Logger, transports } from "winston";
+import { LOGGER_CONFIG } from "../common/constants/config";
+
+type LogMessage = {
+    message:string;
+    metadata:Record<string,unknown>[];
+}
+
+class LoggerService{
+    private _logger:Logger = createLogger({
+        level:LOGGER_CONFIG.MIN_LEVEL,
+        transports:[
+            new transports.Console({
+                format: format.combine(
+                    format.colorize(),
+                    format.timestamp({
+                        format:'YYYY-MM-DD HH:mm:ss A'
+                    }),
+                    format.printf((info) => `[${info.timestamp}] ${info.level}: [${info.provider}] ${info.message}`)
+                )
+            }),
+            new transports.File({
+                filename:"error.log",
+                level:"error",
+                dirname:LOGGER_CONFIG.LOG_DIRNAME,
+                format:format.combine(
+                    format.json(),
+                    format.timestamp({
+                        format: 'YYYY-MM-DD HH:mm:ss A'
+                    }),
+                    format.prettyPrint()
+                ),
+            }),
+            new transports.File({
+                filename:"warn.log",
+                level:"warn",
+                dirname:LOGGER_CONFIG.LOG_DIRNAME,
+                format:format.combine(
+                    format.json(),
+                    format.timestamp({
+                        format: 'YYYY-MM-DD HH:mm:ss A'
+                    }),
+                    format.prettyPrint()
+                )
+            })
+        ]
+    });
+
+    private static _instance:LoggerService;
+
+    private constructor(){}
+
+    public static getInstance():LoggerService{
+        if(!LoggerService._instance){
+            this._instance = new LoggerService();
+        }
+
+        return LoggerService._instance;
+    }
+
+    public info({message,metadata}:LogMessage){
+        return this._logger.info(message,...metadata);
+    }
+
+    public error({message,metadata}:LogMessage){
+        return this._logger.error(message,...metadata);
+    }
+
+    public warning({message,metadata}:LogMessage){
+        return this._logger.warn(message,...metadata);
+    }
+}
+
+export { LoggerService };
